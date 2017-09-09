e7a7429365d05a61ce8256243ac1ee706917a48e
David Aviles <aviles.david1@gmail.com>
2017-09-09 19:22:26 -0400

    added styles, image, and core html

--- index.php
+++ index.php
@@ -6,12 +6,14 @@
     <meta name="viewport" content="width=device-width", initial-scale=1>
 	<title>Harvard User Profile Design in PHP</title>
 	<meta name="robots" content="noindex,nofollow">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans|Titillium+Web" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="/css/application.css">
 </head>
 <body>
     <div class="profile-wrp">
     	<div class="user">
 	    	<div class="user-img">
-				<img src="" alt="David Aviles">
+				<img src="/img/me.jpg" alt="David Aviles">
 	    	</div>
 	    	<div class="user-details">
 				<h1>David Aviles</h1>
@@ -21,12 +23,10 @@
 	    </div>
 	    <div class="user-extended">
 			<h2>About Me</h2>
-			<p></p>
+			<p>I've had an interest in technology since I was young. As I got older, I found the web to be my true calling. During my undergraduate studies I specialized in web applications and the hardware that runs them. Today, I get to build awesome applications and features for really cool companies. I look forward to learning more in this class.</p>
 			<h2>Quotes I Like</h2>
-			<p></p>
-	    </div>
-	    <div class="contact">
-	    	<a href="mailto: daa282@g.Harvard.edu">Contact Me</a>
+			<p>The day soldiers stop bringing you their problems is the day you have stopped leading them. They have either lost confidence that you can help them or concluded that you do not care. Either case is a failure of leadership.</p>
+			<p class="quote-attribution">- Colin Powell</p>
 	    </div>
     </div>
 </body>
