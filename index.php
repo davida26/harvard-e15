69a83dd3bde45ceb0bd95e5ad5426bee10b3d8ae
David Aviles <aviles.david1@gmail.com>
2017-09-09 14:22:51 -0400

    added base index.php and sass configuration

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie-edge">
+    <meta name="viewport" content="width=device-width", initial-scale=1>
+	<title>Harvard User Profile Design in PHP</title>
+	<meta name="robots" content="noindex,nofollow">
+</head>
+<body>
+    <div class="profile-wrp">
+    	<div class="user">
+	    	<div class="user-img">
+				<img src="" alt="David Aviles">
+	    	</div>
+	    	<div class="user-details">
+				<h1>David Aviles</h1>
+				<p class="work">Software Engineer @IWH</p>
+				<p class="location">Greater New York City Area</p>
+	    	</div>
+	    </div>
+	    <div class="user-extended">
+			<h2>About Me</h2>
+			<p></p>
+			<h2>Quotes I Like</h2>
+			<p></p>
+	    </div>
+	    <div class="contact">
+	    	<a href="mailto: daa282@g.Harvard.edu">Contact Me</a>
+	    </div>
+    </div>
+</body>
+</html>
