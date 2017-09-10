eaeb99849f58a2555a2d4fc75bcfb8b99f842e5f
David Aviles <aviles.david1@gmail.com>
2017-09-10 13:36:04 -0400

    w3c validation on index.php completed, no issues

--- index.php
+++ index.php
@@ -2,11 +2,12 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie-edge">
-    <meta name="viewport" content="width=device-width", initial-scale=1>
+    <meta http-equiv="x-ua-compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Harvard User Profile Design in PHP</title>
 	<meta name="robots" content="noindex,nofollow">
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans|Titillium+Web" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Titillium+Web" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="/css/application.css">
 </head>
 <body>
