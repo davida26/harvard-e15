a3abc6e59477d4df2185744239558c00c52c0b11
David Aviles <aviles.david1@gmail.com>
2017-09-10 21:20:43 -0400

    updated multi-dimensional arrays and variables

--- quote.php
+++ quote.php
@@ -1,16 +1,28 @@
 <?php
 
-# Generates a Random Number
+# Generates a Random Number 
+$randomNumber = rand(0,3);
 
-$randomNumber = rand(0,2);
-
+# Quotes Array
 $quoteArray = array(
-	array('First do it, then do it right, then do it better.', 'Addy Osmani' ),
-	array('Success is not final, failure is not fatal: it is the courage to continue that counts.', '- Winston Churchill' ),
-	array('Whatever the mind of man can conceive and believe, it can achieve.', 'Napoleon Hill' ),
-	array('Whether you think you can, or you think you can’t– you’re right.', 'Henry Ford' )
+    0 => array(
+        'quote' => 'First do it, then do it right, then do it better.',
+        'author' => 'Addy Osmani'
+    ),
+    1 => array(
+        'quote' => 'Success is not final, failure is not fatal: it is the courage to continue that counts.',
+        'author' => 'Winston Churchill'
+    ),
+    2 => array(
+        'quote' => 'Whatever the mind of man can conceive and believe, it can achieve.',
+        'author' => 'Napoleon Hill'
+    ),
+    3 => array(
+        'quote' => 'Whether you think you can, or you think you can’t– you’re right.',
+        'author' => 'Henry Ford'
+    )
+);
 
-	);
-
-$getQuote = $quoteArray[$randomNumber][0];
-$getQuoteAuthor = $quoteArray[$randomNumber][0][0];
+# Output Variables
+$getQuote = $quoteArray[$randomNumber]['quote'];
+$getQuoteAuthor = $quoteArray[$randomNumber]['author'];
